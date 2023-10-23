97b49479d4defd549381b526683ef9a107f3e111
gabriellend <gdvdsn@gmail.com>
2023-10-23 12:39:01 -1000

    Begin modularization

--- index.js
+++ index.js
@@ -1,6 +1,40 @@
-// grab the gamecells
-// iterate
-// alternate putting xs and os in them
+const Gameboard = () => {
+  const rows = 3;
+  const columns = 3;
+  const board = [];
+
+  for (let i = 0; i < rows; i++) {
+    board[i] = [];
+    for (let j = 0; j < columns; j++) {
+      board[i].push(Cell());
+    }
+  }
+
+  const getBoard = () => board;
+
+  const fillCell = (cell, player) => {
+
+  }
+
+  return {
+    getBoard,
+  }
+}
+
+function Cell() {
+  let value = null;
+
+  const addValue = (player) => {
+    value = player;
+  };
+
+  const getValue = () => value;
+
+  return {
+    addValue,
+    getValue
+  };
+}
 
 const renderGameboard = () => {
   const gamecells = document.querySelectorAll(".gamecell");
