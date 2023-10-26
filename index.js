4a9c401144ba7de1721ad79e1c36d70ef4966a8c
gabriellend <gdvdsn@gmail.com>
2023-10-25 14:25:00 -1000

    Add GameController

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+// pressing start initiates the game
+// event handler -> GameController.start
+// GameController.start
+// unlock the board
+// player x goes first
+// grab all squares (ScreenController?)
+// click is registered on a square
+// if the square is not already occupied
+// place an x there
+// computer is player o and goes second
+// after one player has made three moves
+// we start checking for wins and ties
 const Gameboard = () => {
   const rows = 3;
   const columns = 3;
@@ -12,41 +24,85 @@ const Gameboard = () => {
 
   const getBoard = () => board;
 
-  const fillCell = (cell, player) => {
-
-  }
+  const markBoard = (cell, playerToken) => {
+    if (!cell.getValue()) {
+      cell.setValue(playerToken);
+    }
+  };
 
   return {
     getBoard,
-  }
-}
+    markBoard,
+  };
+};
 
 function Cell() {
   let value = null;
 
-  const addValue = (player) => {
+  const setValue = (player) => {
     value = player;
   };
 
   const getValue = () => value;
 
   return {
-    addValue,
-    getValue
+    setValue,
+    getValue,
   };
 }
 
-const renderGameboard = () => {
-  const gamecells = document.querySelectorAll(".gamecell");
+const GameController = (
+  playerOneName = "Player",
+  playerTwoName = "Computer"
+) => {
+  const board = Gameboard();
+  const players = [
+    {
+      name: playerOneName,
+      token: "x",
+    },
+    {
+      name: playerTwoName,
+      token: "o",
+    },
+  ];
+  let activePlayer = players[0];
 
-  gamecells.forEach((cell, i) => {
-    if (i % 2 === 0) {
-      cell.style.backgroundImage = "url(assets/x.png)"
-    } else {
-      cell.style.backgroundImage = "url(assets/o.png)"
-    }
-  });
-}
+  const getActivePlayer = () => activePlayer;
 
-renderGameboard();
+  const switchPlayer = () => {
+    activePlayer = activePlayer === players[0] ? players[1] : players[0];
+  };
 
+  const playTurn = (cell) => {
+    board.markBoard(cell, getActivePlayer().token);
+
+    /*  This is where we would check for a winner and handle that logic,
+        such as a win message. */
+
+    switchPlayer();
+  };
+
+  return {
+    playTurn,
+    getActivePlayer,
+    getBoard: board.getBoard,
+  };
+};
+
+const ScreenController = () => {
+  const game = GameController();
+};
+// const renderGameboard = () => {
+//   const gamecells = document.querySelectorAll(".gamecell");
+
+//   gamecells.forEach((cell, i) => {
+//     if (i % 2 === 0) {
+//       cell.style.backgroundImage = "url(assets/x.png)"
+//     } else {
+//       cell.style.backgroundImage = "url(assets/o.png)"
+//     }
+//   });
+// }
+
+// renderGameboard();
